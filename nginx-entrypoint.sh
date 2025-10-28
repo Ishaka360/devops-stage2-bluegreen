5f49843819e1dc39cbe2b835154f3d6bd337c374
Ishaka Michael Efe <ishakamichaelefe@gmail.com>
2025-10-28 08:01:17 +0100

    Create nginx-entrypoint.sh

---
+++ nginx-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$ACTIVE_COLOR" ]; then
+  echo "ACTIVE_COLOR not set, defaulting to blue"
+  ACTIVE_COLOR=blue
+fi
+
+echo "Starting Nginx with active color: $ACTIVE_COLOR"
+
+envsubst '$ACTIVE_COLOR' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp
+mv /etc/nginx/conf.d/default.conf.tmp /etc/nginx/conf.d/default.conf
+
+nginx -g "daemon off;"
