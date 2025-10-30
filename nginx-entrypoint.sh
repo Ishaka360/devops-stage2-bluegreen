0ec02ca0640eca6bc492f654bdcc0baff5c6ca3e
Ishaka Michael Efe <ishakamichaelefe@gmail.com>
2025-10-30 09:28:28 +0100

    Update nginx-entrypoint.sh

--- nginx-entrypoint.sh
+++ nginx-entrypoint.sh
@@ -3,7 +3,8 @@ set -e
 
 echo "Starting Nginx with active color: ${ACTIVE_COLOR}"
 
-# Replace the active color placeholder in the template
-envsubst '${ACTIVE_COLOR}' < /nginx.template > /etc/nginx/conf.d/default.conf
+# Generate nginx configuration from template
+envsubst '${ACTIVE_COLOR} ${BLUE_PORT} ${GREEN_PORT}' < /nginx.template > /etc/nginx/conf.d/default.conf
 
+# Start nginx in the foreground
 nginx -g 'daemon off;'
